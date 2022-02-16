bc27704516a6cd26c00bf6fc06580fa1a64125c9
Luis Fernando Useche <Arqsunny@users.noreply.github.com>
2022-02-16 21:05:42 +0100

    q

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-meeting-room",
-      "name": "Meeting Room",
+      "id": "0-360_0467_stitch_xhc",
+      "name": "360_0467_Stitch_XHC",
       "levels": [
         {
           "tileSize": 256,
@@ -22,18 +22,18 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1728,
+      "faceSize": 1368,
       "initialViewParameters": {
-        "yaw": 1.7025107797454968,
-        "pitch": 0.38318946642048957,
-        "fov": 1.5338614860544868
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "1-dinner",
-      "name": "Dinner",
+      "id": "1-360_0468_stitch_xhc",
+      "name": "360_0468_Stitch_XHC",
       "levels": [
         {
           "tileSize": 256,
@@ -53,18 +53,18 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1728,
+      "faceSize": 1368,
       "initialViewParameters": {
-        "yaw": -1.7213873130661934,
-        "pitch": 0.6841413534946881,
-        "fov": 1.5338614860544868
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-happy-hour",
-      "name": "Happy Hour",
+      "id": "2-360_0469_stitch_xhc",
+      "name": "360_0469_Stitch_XHC",
       "levels": [
         {
           "tileSize": 256,
@@ -84,11 +84,228 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1728,
+      "faceSize": 1368,
       "initialViewParameters": {
-        "yaw": -2.6459657367475344,
-        "pitch": 0.28493377094187977,
-        "fov": 1.5338614860544868
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-360_0470_stitch_xhc",
+      "name": "360_0470_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-360_0471_stitch_xhc",
+      "name": "360_0471_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-360_0472_stitch_xhc",
+      "name": "360_0472_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-360_0473_stitch_xhc",
+      "name": "360_0473_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-360_0474_stitch_xhc",
+      "name": "360_0474_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-360_0475_stitch_xhc",
+      "name": "360_0475_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-360_0476_stitch_xhc",
+      "name": "360_0476_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
